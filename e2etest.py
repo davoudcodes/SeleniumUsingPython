19657f9675e2c66b89f2acde705aff9d4ea4f0c1
Davoud Yousuf <128808526+davoudcodes@users.noreply.github.com>
2025-02-01 15:04:30 +0530

    added chaining part

--- e2etest.py
+++ e2etest.py
@@ -11,4 +11,13 @@ driver.implicitly_wait(4)  #implicit wait
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
 #//a[contains(@href,'shop')]
+
+products = driver.find_elements(By.XPATH,"//app-card")
+
+for product in products:
+    productName = product.find_element(By.XPATH, "div/div/h4/a").text
+    if productName == "Blackberry":
+        product.find_element(By.XPATH, "div/div/button").click()
+
+driver.find_element(By.XPATH, "//div/ul/li/a").click()
 time.sleep(2)
